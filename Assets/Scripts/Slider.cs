43a321058a5eba1f15e3da70998be12e91afe88f
y33550336 <u5yuugoo@gmail.com>
2026-05-10 02:10:32 +0900

    Fix: Clear tracking of ignored files

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -24,47 +24,47 @@ public class Slider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(locked && Input.GetKeyDown(KeyCode.R))
+        if (locked && Input.GetKeyDown(KeyCode.R))
         {
             Initialize();
         }
 
-        if(locked) return;
+        if (locked) return;
 
-        if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             MovePlayer(0, -1, PlayerTurn);
         }
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             MovePlayer(-1, 0, PlayerTurn);
         }
-        if(Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             MovePlayer(0, 1, PlayerTurn);
         }
-        if(Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             MovePlayer(1, 0, PlayerTurn);
         }
-    
+
     }
 
-    void MovePlayer(int dx, int dy, int playerIndex) 
+    void MovePlayer(int dx, int dy, int playerIndex)
     {
-        if(playerPositions[playerIndex].x + dx < 0 || playerPositions[playerIndex].x + dx >= boardSize)return;
-        if(playerPositions[playerIndex].y + dy < 0 || playerPositions[playerIndex].y + dy >= boardSize)return;
-        if(cellValues[playerPositions[playerIndex].x + dx, playerPositions[playerIndex].y + dy] == 3)
+        if (playerPositions[playerIndex].x + dx < 0 || playerPositions[playerIndex].x + dx >= boardSize) return;
+        if (playerPositions[playerIndex].y + dy < 0 || playerPositions[playerIndex].y + dy >= boardSize) return;
+        if (cellValues[playerPositions[playerIndex].x + dx, playerPositions[playerIndex].y + dy] == 3)
         {
             cellValues[playerPositions[playerIndex].x, playerPositions[playerIndex].y] = 3;
             cellValues[playerPositions[playerIndex].x + dx, playerPositions[playerIndex].y + dy] = 1;
             playerPositions[playerIndex].x += dx;
             playerPositions[playerIndex].y += dy;
-            
+
             players[playerIndex].transform.position += new Vector3(dx, -dy, 0);
         }
-        
-        else if(cellValues[playerPositions[playerIndex].x + dx, playerPositions[playerIndex].y + dy] == 0)
+
+        else if (cellValues[playerPositions[playerIndex].x + dx, playerPositions[playerIndex].y + dy] == 0)
         {
             // スライダーを動かす
             bool canMove = true;
@@ -72,13 +72,13 @@ public class Slider : MonoBehaviour
             int currentSliderValue = sliderValues[playerPositions[playerIndex].x, playerPositions[playerIndex].y];
             // 今乗っているスライダーの値を-1にして、スライダーが動いた後に更新する
             sliderValues[playerPositions[playerIndex].x, playerPositions[playerIndex].y] = -1;
-            while(canMove)
+            while (canMove)
             {
                 canMove = false;
                 // スライダーが動けるか確認
-                if(playerPositions[playerIndex].x + dx < 0 || playerPositions[playerIndex].x + dx >= boardSize)break;
-                if(playerPositions[playerIndex].y + dy < 0 || playerPositions[playerIndex].y + dy >= boardSize)break;
-                if(cellValues[playerPositions[playerIndex].x + dx, playerPositions[playerIndex].y + dy] == 0)
+                if (playerPositions[playerIndex].x + dx < 0 || playerPositions[playerIndex].x + dx >= boardSize) break;
+                if (playerPositions[playerIndex].y + dy < 0 || playerPositions[playerIndex].y + dy >= boardSize) break;
+                if (cellValues[playerPositions[playerIndex].x + dx, playerPositions[playerIndex].y + dy] == 0)
                 {
                     // セルの値を更新
                     cellValues[playerPositions[playerIndex].x, playerPositions[playerIndex].y] = 0;
@@ -86,7 +86,7 @@ public class Slider : MonoBehaviour
                     // プレイヤーの位置を更新
                     playerPositions[playerIndex].x += dx;
                     playerPositions[playerIndex].y += dy;
-    
+
                     // プレイヤーとスライダーの表示位置を更新
                     sliders[currentSliderValue].transform.position += new Vector3(dx, -dy, 0);
                     players[playerIndex].transform.position += new Vector3(dx, -dy, 0);
@@ -101,7 +101,7 @@ public class Slider : MonoBehaviour
 
     void EndTurn()
     {
-        if(playerPositions[PlayerTurn].x  == boardSize / 2 && playerPositions[PlayerTurn].y == boardSize / 2)
+        if (playerPositions[PlayerTurn].x == boardSize / 2 && playerPositions[PlayerTurn].y == boardSize / 2)
         {
             Debug.Log("Player " + (PlayerTurn + 1) + " wins!");
             locked = true;
@@ -145,19 +145,19 @@ public class Slider : MonoBehaviour
         sliders[5].transform.position = cells[13].transform.position + new Vector3(0, 0, -0.1f);
         sliders[6].transform.position = cells[35].transform.position + new Vector3(0, 0, -0.1f);
         sliders[7].transform.position = cells[41].transform.position + new Vector3(0, 0, -0.1f);
-        sliders[8].transform.position = cells[42].transform.position + new Vector3( 0, 0, -0.1f);
+        sliders[8].transform.position = cells[42].transform.position + new Vector3(0, 0, -0.1f);
         sliders[9].transform.position = cells[43].transform.position + new Vector3(0, 0, -0.1f);
         sliders[10].transform.position = cells[47].transform.position + new Vector3(0, 0, -0.1f);
         sliders[11].transform.position = cells[48].transform.position + new Vector3(0, 0, -0.1f);
 
-        
+
         // プレイヤーの初期位置を設定
         playerPositions[0] = new Vector2Int(0, 0);
         playerPositions[1] = new Vector2Int(6, 6);
 
         players[0].transform.position = cells[0].transform.position + new Vector3(0, 0, -0.2f);
         players[1].transform.position = cells[48].transform.position + new Vector3(0, 0, -0.2f);
-        
+
         // プレイヤー1のターンから開始
         PlayerTurn = 0;
         locked = false;
