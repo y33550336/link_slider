db8816e10bfb7e92307758f15bc20f47392d1e9d
Taisei <libertegra@gmail.com>
2026-06-21 02:37:09 +0900

    add : 待った の機能追加。

--- Assets/Scripts/SceneTraveler.cs
+++ Assets/Scripts/SceneTraveler.cs
@@ -4,7 +4,7 @@ public static class SceneTraveler
 {
     //ゲーム起動中において、変数をシーン間で共有するためのスタティッククラス
     public static int playerNumber = 2; //プレイヤーの数
-
+    public static bool Backable = true; //待ったが可能かどうか
     public static void SetPlayerNumber(int number)
     {
         playerNumber = number;
@@ -13,5 +13,15 @@ public static class SceneTraveler
     public static int GetPlayerNumber()
     {
         return playerNumber;
+    }
+
+    public static void SetBackable(bool backable)
+    {
+        Backable = backable;
+    }
+
+    public static bool GetBackable()
+    {
+        return Backable;
     }
 }
